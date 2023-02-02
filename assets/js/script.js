b39955d1b2cdd8cf9ad338b6365182684c9974ec
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-02 21:25:51 +0000

    Corrected query url to get results from the first input

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,24 +1,23 @@
+const apiKey = 'yHlL3BnAnm05Y3qa3ROr4iaSSxRxY7qF';
 
 const searchButton = $("#search-button");
 const clearhButton = $("#clear-button");
 
 searchButton.on('click', function(event) {
-    let searchTermsEl = $("#search-terms").val().trim().toString();
+    let searchTermsEl = $("#search-terms").val().trim();
     console.log(searchTermsEl);
+    
     let numberOfRecordsEl = $("#number-of-records").val().trim();
     let startEl = $("#start-year").val().trim();
     let endEl = $("#end-year").val().trim();
 
-    let queryURL = "https://api.nytimes.com/svc/search/v2/articlesearch.json?q=" +
-    searchTermsEl +
-    + "&api_key=DtRjFGARJtsY8PTXWLRt0AUqlNkHqzD0";
+    let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTermsEl}&api-key=${apiKey}`;
 
-
+    console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        let results = response.data;
-        console.log(results)
+        console.log(response.response.docs);
     });
 })
