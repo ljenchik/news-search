3165a8f3589437173e21f69df3dd6a39af81e1f6
Olena Pelagenko <84686704+ljenchik@users.noreply.github.com>
2023-02-02 22:01:59 +0000

    Added search if all inputs are entered

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,4 +1,5 @@
 const apiKey = 'yHlL3BnAnm05Y3qa3ROr4iaSSxRxY7qF';
+let queryURL;
 
 const searchButton = $("#search-button");
 const clearhButton = $("#clear-button");
@@ -7,17 +8,26 @@ searchButton.on('click', function(event) {
     let searchTermsEl = $("#search-terms").val().trim();
     console.log(searchTermsEl);
     
-    let numberOfRecordsEl = $("#number-of-records").val().trim();
+    let numberOfRecordsEl = parseInt($("#number-of-records").val().trim());
     let startEl = $("#start-year").val().trim();
+    startEl = startEl+"0101"
     let endEl = $("#end-year").val().trim();
+    endEl = endEl + "1231"
 
-    let queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTermsEl}&api-key=${apiKey}`;
+    if (searchTermsEl) {
+        queryURL = `https://api.nytimes.com/svc/search/v2/articlesearch.json?q=${searchTermsEl}&api-key=${apiKey}&begin_date=${startEl}&end_date=${endEl}`;
+    }
+    else {
+        alert("Enter search terms");
+    }
+
+    
 
     console.log(queryURL);
     $.ajax({
         url: queryURL,
         method: "GET",
     }).then(function (response) {
-        console.log(response.response.docs);
+        console.log(response.response.docs.slice(0, numberOfRecordsEl));
     });
 })
